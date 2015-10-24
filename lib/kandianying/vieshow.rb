a6383d48d79f75fe3d7d89e1437a3eba049a0948
pengyuchen <pengyu@nlplab.cc>
2015-10-24 20:26:30 +0800

    most of rubocop

--- lib/kandianying/vieshow.rb
+++ lib/kandianying/vieshow.rb
@@ -2,52 +2,43 @@ require 'open-uri'
 require 'nokogiri'
 require 'json'
 
-# url   = 'http://sales.vscinemas.com.tw/ticketing/visPrintShowTimes.aspx?visCinemaID=0005'
-# open_doc = open(url, ,"Cookie" => "AspxAutoDetectCookieSupport=1")
-# table = doc.xpath("//*[@class='PrintShowTimesTable']//td[@class='PrintShowTimesFilm' or @class='PrintShowTimesDay' or @class='PrintShowTimesSession']")
-# table[0].first[1]
-#  {"name"=>[[day,time],[day,time],[day,time],[day,time]],"name2".....}
-
-
-# Scraper for VieShow 
+# Scraper for VieShow
 module VieShow
-  
-  #initialize movie_table with url  
+  # initialize movie_table with url
   def initialize(visCinemaID)
     url = 'http://sales.vscinemas.com.tw/ticketing/visPrintShowTimes.aspx?visCinemaID=' + visCinemaID
-    open_doc = open(url,"Cookie" => "AspxAutoDetectCookieSupport=1")
+    open_doc = open(url, 'Cookie' => 'AspxAutoDetectCookieSupport=1')
     doc = Nokogiri::HTML(open_doc)
-    @table = doc.xpath("//*[@class='PrintShowTimesTable']//td[@class='PrintShowTimesFilm' or @class='PrintShowTimesDay' or @class='PrintShowTimesSession']")
-    @movie_table   = {}
+    @table = doc.xpath("//*[@class='PrintShowTimesTable']//td[@class='PrintShowTimesFilm' "\
+                       "or @class='PrintShowTimesDay' "\
+                       "or @class='PrintShowTimesSession']")
+    @movie_table = {}
+    make_movie_table
   end
 
-
-  # make movie_table like the structure {"name"=>[[day,time],[day,time],[day,time],[day,time]],"name2".....}
-  def get_movie_table    
+  # make movie_table like the structure {"name"=>[[day,time],"name2".....}
+  def make_movie_table
     current_movie = ''
     @table.each do |td|
-      if    td.first[1] == "PrintShowTimesFilm"
+      case td.first[1]
+      when 'PrintShowTimesFilm'
         @movie_table[td.text] = []
-        current_movie        = td.text
-      elsif td.first[1] == "PrintShowTimesDay"
+        current_movie = td.text
+      when 'PrintShowTimesDay'
         @movie_table[current_movie] << [td.text]
-      elsif td.first[1] == "PrintShowTimesSession"
-        @movie_table[current_movie][-1] << td.text 
-      end  
+      when 'PrintShowTimesSession'
+        @movie_table[current_movie][-1] << td.text
+      end
     end
-    @movie_table
   end
 
   # get all movies' names
-  def get_movie_name
-    get_movie_table
+  def make_movie_name
     @movie_table.keys
   end
 
   # get json format from movie_table
   def to_json
-    get_movie_table
     @movie_table.to_json
   end
-  
 end
