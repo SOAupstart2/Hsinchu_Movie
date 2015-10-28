34cbc80bca44eb8d9ceaaa1f28d39b5a2bb88465
pengyuchen <pengyu@nlplab.cc>
2015-10-28 09:30:24 +0800

    add scraper for ambassador

--- lib/kandianying.rb
+++ lib/kandianying.rb
@@ -1,6 +1,8 @@
 require_relative './kandianying/vieshow'
+require_relative './kandianying/ambassador'
 
 # class for crawling movie information
 class HsinChuMovie
   include VieShow
+  include Ambassador
 end
