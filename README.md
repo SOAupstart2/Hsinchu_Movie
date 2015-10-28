e00f7ad90cefcb03c507f2fac2102e583e709881
James Uanhoro <stonegold546@gmail.com>
2015-10-28 09:42:49 +0800

    Made further changes to vieshow module
    - Code wasn't working previously :(, all cinema codes were retrieving same resu$
    - Scraping cinema name
    - Changed bin to reflect changes
    - Modified README to reflect these changes
    - Need to write tests, a bit ass backwards :(

--- README.md
+++ README.md
@@ -2,37 +2,33 @@
 
 https://rubygems.org/gems/kandianying
 
-A simple Ruby Gem to scrap the website vieshow and to get film list and its dates and time.
+A simple Ruby Gem to get data from cinema websites in Hsinchu. Currently, we can get the film lists and show times given a vieshow cinema code.
 
-
-## Usage
+## Installation
 
 Run the following command:
-
 ```
 gem install kandianying
 ```
+
+### General usage notes
+Currently, usage requires specifying a theater_id. This value ranges from 1 to 14. For a cinema located in Hsinchu, use 5 or 12.
+
 ### Command line usage
-
 ```
-kandianying theater id
+kandianying vieshow <theater_id>
+# This prints out cinema name with films on display and show times.
 ```
-
-Puts information of films to command line,
-- vieshow
-  - 0005 is for VS Cinemas Hsinchu FE21  
-  - 0012 is for Vie Show Cinemas Hsinchu Big City
 
 ### Usage in ruby code
 ```
 require 'kandianying'
 
-vieshow_movie = HsinChuMovie::Vieshow.new('0005')
+vieshow_movie = HsinChuMovie::Vieshow.new(<theater_id>)
 vieshow_movie.movie_names  # Returns array of film names
 vieshow_movie.movie_table  # Returns JSON array of film names, dates, time
 vieshow_movie.to_json  # Puts JSON array of movie_table
 ```
 
 ## License
-
 Distributed under the [MIT License](LICENSE).
--- lib/kandianying/vieshow.rb
+++ lib/kandianying/vieshow.rb
@@ -4,9 +4,8 @@ require 'json'
 
 # Scraper for VieShow
 module VieShow
-  VIESHOW_MAPPING = {'0005'=>'Vieshow_HsinChuFE21', '0012'=>'Vieshow_BigCity'} 
   VIESHOW_URL = 'http://sales.vscinemas.com.tw/ticketing/visPrintShowTimes'\
-                '.aspx?visCinemaID='
+                '.aspx?visCinemaID=00'
   VIESHOW_COOKIE_SETTINGS = 'AspxAutoDetectCookieSupport=1'
   VIESHOW_BASE_XPATH = 'PrintShowTimes'
   VIESHOW_CLASSES_XPATH = %w(Film Day Session)
@@ -18,23 +17,28 @@ module VieShow
   VIESHOW_CINEMA_CODES = ('1'..'14').to_a.map do |e|
     e.length == 1 ? "0#{e}" : e
   end
+  VIESHOW_CINEMA_NAME_XPATH = "//*[contains(@class, 'PrintCinemaName')]"
 
   # Class for Vieshow films
   class Vieshow
     # initialize movie_table with ID
-    def initialize(visCinemaID)
-      doc = visit_vieshow(visCinemaID)
+    def initialize(vis_cinema_id)
+      vis_cinema_id = vis_cinema_id.to_s
+      vis_cinema_id = "0#{vis_cinema_id}" if vis_cinema_id.length == 1
+      fail unless VIESHOW_CINEMA_CODES.include? vis_cinema_id
+      doc = visit_vieshow(vis_cinema_id)
       @table = doc.xpath(VIESHOW_FULL_XPATH)
+      @cinema_name = doc.xpath(VIESHOW_CINEMA_NAME_XPATH).text
       @movie_table = Hash.new { |hash, key| hash[key] = [] }
       make_movie_table
-      @movie_table = { VIESHOW_MAPPING[visCinemaID] => @movie_table }
+      @movie_table = { @cinema_name => @movie_table }
     end
 
     # fetch movie_table
-    attr_reader :movie_table
+    attr_reader :movie_table, :cinema_name
 
-    def visit_vieshow(visCinemaID)
-      url = VIESHOW_URL + visCinemaID
+    def visit_vieshow(vis_cinema_id)
+      url = VIESHOW_URL + vis_cinema_id
       open_doc = open(url, 'Cookie' => VIESHOW_COOKIE_SETTINGS)
       Nokogiri::HTML(open_doc)
     end
